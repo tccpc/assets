3faf4fed24988cd8545b0dbf576d485d395b4b6f
tccpc <648593412@qq.com>
2020-06-14 00:39:38 +0800

    add

--- video/export.js
+++ video/export.js
@@ -3,13 +3,13 @@ var fs = require('fs');
 let components = []
 const files = fs.readdirSync('./')
 files.forEach(function (item, index) {
-  if (item === '.DS_Store' || item === 'export.js') return
+  if (item === '.DS_Store' || item === 'export.js'|| item === 'bgurl.json') return
     components.push(item)
 })
 
 let str = JSON.stringify(components)
  
- fs.writeFile('./bgurl.json',str,function(err){
+ fs.writeFile('./videoUrl.json',str,function(err){
  if (err) {
      console.log('error');
  }
